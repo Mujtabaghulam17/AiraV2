fd97bf3daa237b8acb71b9a8de306c422b20b7ea
Mujtabaghulam17 <109437580+Mujtabaghulam17@users.noreply.github.com>
2025-10-04 21:36:54 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,57 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  reactStrictMode: true,
+  poweredByHeader: false,
+  compress: true,
+  
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+  
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          }
+        ]
+      }
+    ];
+  },
+  
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.aira-ai.nl',
+          },
+        ],
+        destination: 'https://aira-ai.nl/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
